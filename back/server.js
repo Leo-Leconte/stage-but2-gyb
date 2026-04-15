b9d7bedcc8807e17ce19b2c65fa6450a62a55b5b
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-15 11:34:15 +0200

    :art: Modification du lancement du server pour plus de securite et de pouvoir effectuer des test par derriere

--- back/server.js
+++ back/server.js
@@ -6,8 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/api/test', (req, res) => res.status(200).json({ message: 'Bien recu' }))
+app.get('/api/test', (req, res) => res.status(200).json({ message: 'Bien reçu' })) // permet de tester le serveur
 
-app.listen(port, () => console.log(`Serveur sur ecoute sur le port ${port}`));
+if (require.main === module) { // permet de lancer le serveur afin de differencier qui le lance et de pouvoir lancer le serveur en local et effectuer des test dessus
+    app.listen(port, () => console.log(`Serveur sur ecoute sur le port ${port}`)); // permet de lancer le serveur et d avoir un log par derriere
+}
+
+module.exports = app; // permet de lancer le serveur
