a32e4c9f0be301f8e82c86315dfb6053ace07222
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-16 16:55:18 +0200

    :zap: Amelioration du code grace a SonarQubbe

--- back/route/login.route.js
+++ back/route/login.route.js
@@ -1,11 +1,6 @@
-const app = require ('express');
-const { Request, Response } = require ('express');
-const { auth } = require ('../Middleware/auth');
-const { Control } = require ('../Controller/authController');
 const pool = require ('../src/db');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const {sign} = require("jsonwebtoken");
 const router = require ('express').Router();
 
 router.post("/login", async (req, res) => {
@@ -27,11 +22,10 @@ router.post("/login", async (req, res) => {
             return res.status(401).json({ message: "Collaborateur inconnu." });
         }
 
-        const user = result.rows[0];
         const ok = await bcrypt.compare(mot_de_passe, collaborateur.mot_de_passe);
 
         if (!ok) {
-            return res.status(401).json({ message: "Nom ou Mot de passe incorrect." }); // toujours dire que le nom ou le mot de passe est incorrect pour eviter les attaques par brute force
+            return res.status(401).json({ message: "Nom ou Mot de passe incorrect." }); // toujours dire que le nom ou le mot de passe est incorrect pour éviter les attaques par brute force
         }
 
         const token = jwt.sign(
