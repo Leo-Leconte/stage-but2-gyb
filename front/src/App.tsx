e53d86776d4b77cade9f1fc44ddd3cef8caedeb1
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-16 11:12:25 +0200

    #4 configuration pour avoir le composant Login à la route racine (/), pas besoin de Home car quelqu'un de non collaborateur n'est pas censé voir de menu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Login from '../src/components/login/login.tsx'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
-      <>
-          <Login />
-      </>
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/"} element={<Login/>} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
--- front/src/components/login/login.tsx
+++ front/src/components/login/login.tsx
@@ -1,30 +1,34 @@
-//import styles from './Login.module.css';
-//import { useState } from 'react'
-
+import styles from './Login.module.css';
+// import { useState } from 'react'
 
 const Login = () => {
-
     return (
-        <>
-            <div className="bg">
-                <div className="shape"></div>
-                <div className="shape"></div>
-            </div>
-            <form>
-                <h2>Connectez-vous</h2>
+            <div className={styles.bg}>
+            <form className={styles.form}>
+                <h2 className={styles.title}>Connectez-vous</h2>
 
-                <label>Nom d'utilisateur</label>
-                <input type="text" placeholder="Nom d'utilisateur" id="username"/>
+                <label className={styles.label} htmlFor="username">Nom d'utilisateur</label>
+                <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Nom d'utilisateur"
+                    id="username"
+                />
 
-                <label>Mot de passe</label>
-                <input type="text" placeholder="Mot de passe" id="password"/>
+                <label className={styles.label} htmlFor="password">Mot de passe</label>
+                <input
+                    className={styles.input}
+                    type="password"
+                    placeholder="Mot de passe"
+                    id="password"
+                />
 
-                <button>Se connecter</button>
+                <button className={styles.button} type="submit">
+                    Se connecter
+                </button>
             </form>
-        </>
+        </div>
     );
-
-
-
 }
+
 export default Login;
