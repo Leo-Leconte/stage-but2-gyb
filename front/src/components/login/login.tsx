9e33b11a269abed81800bf60a260da96368aef8c
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-16 10:19:54 +0200

    #4 login s'affiche

--- front/src/components/login/login.tsx
+++ front/src/components/login/login.tsx
@@ -1,1 +1,30 @@
-import styles from './Login.module.css';
+//import styles from './Login.module.css';
+//import { useState } from 'react'
+
+
+const Login = () => {
+
+    return (
+        <>
+            <div className="bg">
+                <div className="shape"></div>
+                <div className="shape"></div>
+            </div>
+            <form>
+                <h2>Connectez-vous</h2>
+
+                <label>Nom d'utilisateur</label>
+                <input type="text" placeholder="Nom d'utilisateur" id="username"/>
+
+                <label>Mot de passe</label>
+                <input type="text" placeholder="Mot de passe" id="password"/>
+
+                <button>Se connecter</button>
+            </form>
+        </>
+    );
+
+
+
+}
+export default Login;
