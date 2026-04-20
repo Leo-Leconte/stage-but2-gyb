5bfccfe2facd2a9aebe1609020f10f14f54739a1
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-20 10:46:20 +0200

    #4 modif de la page de redirection pour ajouter un bouton de déconnection (fonctionnel, avec alert qui renvoie mesage de déco confirmée)

--- front/src/components/testRedirect/testRedirect.tsx
+++ front/src/components/testRedirect/testRedirect.tsx
@@ -1,7 +1,31 @@
+import styles from "../login/Login.module.css";
+import { useNavigate } from "react-router";
+
+
 const Redirect= ( ) => {
 
+    const navigate = useNavigate();
+
+    async function handleClickedButton(e : any) {
+        e.preventDefault(); // Le comportement par défaut d'une page html est de recharger la page et d'envoyer les données dans l'URL, donc il faut enlever cela pour gérer nous même le comportement et afficher sur la page le message sans la recharger.
+
+        localStorage.removeItem("token");
+        navigate('/');
+        alert('Vous avez bien été déconnecté.');
+    }
+
+
+
+
+
     return (
-        <h2>Bravo vous êtes connectés ! Features à venir mais la redirection a fonctionnée donc c'est déjà un bon début non? </h2>
+        <>
+            <h2>Bravo vous êtes connectés ! Features à venir mais la redirection a fonctionnée donc c'est déjà un bon
+            début non? </h2>
+            <button className={styles.button} onClick={handleClickedButton}>
+                Se déconnecter
+            </button>
+        </>
     )
 
 }
