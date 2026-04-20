d3601726a13dc9eda351eeef546e4e0dd62f3f1d
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-20 14:15:46 +0200

    #4 test du mdp en tps réel ajouté

--- front/src/__tests__/Login.test.tsx
+++ front/src/__tests__/Login.test.tsx
@@ -6,9 +6,9 @@ import userEvent from '@testing-library/user-event'
 
 
 describe('Login', () => {
-    it("Le champ nom du formulaire s'affiche", () => {
+    it("Le champ email du formulaire s'affiche", () => {
             render(<MemoryRouter><Login/></MemoryRouter>)
-            screen.getByLabelText("Nom d'utilisateur")
+            screen.getByLabelText("Email d'utilisateur")
         })
     it("Le champ mot de passe du formulaire s'affiche", () => {
         render(<MemoryRouter><Login/></MemoryRouter>)
@@ -18,17 +18,20 @@ describe('Login', () => {
         render(<MemoryRouter><Login/></MemoryRouter>)
         screen.getByRole('button', { name: 'Se connecter' })
     })
-    it('Le champ username est mis à jour en temps réel', async () => {
+    it('Le champ email est mis à jour en temps réel', async () => {
         render(<MemoryRouter><Login/></MemoryRouter>)
         const user = userEvent.setup()
-        const champ = screen.getByLabelText("Nom d'utilisateur");
+        const champ = screen.getByLabelText("Email d'utilisateur");
         await user.type(champ, 'ce que je tape');
         expect(champ).toHaveValue('ce que je tape');
 
-    }
-
-
-
-    )
+    })
+    it('Le champ password est mis à jour en temps réel', async () => {
+            render(<MemoryRouter><Login/></MemoryRouter>)
+            const user = userEvent.setup()
+            const champ = screen.getByLabelText("Mot de passe");
+            await user.type(champ, 'ce que je tape');
+            expect(champ).toHaveValue('ce que je tape');
+        })
  })
 
