d9786cd719576a5ac1d77fed7b9daa71136f0858
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-20 13:46:02 +0200

    #4 premier test sur le Logout

---
+++ front/src/__tests__/Logout.test.tsx
@@ -0,0 +1,10 @@
+import {render, screen} from "@testing-library/react";
+import {MemoryRouter} from "react-router-dom";
+import TestRedirect from "../components/testRedirect/testRedirect.tsx";
+
+describe('Redirect',() => {
+    it("Le bouton se déconnecter du formulaire s'affche", () => {
+        render(<MemoryRouter><TestRedirect/></MemoryRouter>)
+        screen.getByRole('button', {name: 'Se déconnecter'})
+    })
+})
