0ad21838013e7438508a700d63b9b391b4f582ba
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-20 11:21:00 +0200

    #4 config de base pour les tests vitest

--- front/src/components/testRedirect/testRedirect.tsx
+++ front/src/components/testRedirect/testRedirect.tsx
@@ -6,6 +6,7 @@ const Redirect= ( ) => {
 
     const navigate = useNavigate();
 
+
     async function handleClickedButton(e : any) {
         e.preventDefault(); // Le comportement par défaut d'une page html est de recharger la page et d'envoyer les données dans l'URL, donc il faut enlever cela pour gérer nous même le comportement et afficher sur la page le message sans la recharger.
 
