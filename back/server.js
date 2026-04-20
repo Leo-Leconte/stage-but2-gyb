b02e90f35603adaa4fe78bf45b94e5cd87646dc4
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-20 09:27:16 +0200

    :memo: Ajout de la documentation et ajout de la route pour le logout

--- back/server.js
+++ back/server.js
@@ -6,15 +6,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const loginRoute = require('./route/login.route');
 const authRoute = require('./route/auth.route');
+const logoutRoute = require('./route/logout.route');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/api/test', (req, res) => res.send('Hello World!')); // route pour tester le serveur
 app.use('/api/auth',loginRoute); // route pour la connexion
 app.use('/api/auth',authRoute); // route pour le auth
+app.use('/api/auth',logoutRoute);// route pour la déconnexion
 
-// si le main est le module courant alors on lance le serveur
+// si le main est le module courant alors, on lance le serveur
 if (require.main === module) {
     app.listen(port, () => console.log(`Serveur sur ecoute sur le port ${port}`));
 }
