598754b4279bd0c3c669816c349569d9d56966a9
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-22 11:29:00 +0200

    #34 Home modif pour return le Header

--- front/src/components/home/Home.tsx
+++ front/src/components/home/Home.tsx
@@ -1,5 +1,6 @@
 import styles from "../login/Login.module.css";
 import { useNavigate } from "react-router";
+import Header from "../layout/Header.tsx";
 
 /**
  * Composant de la page de redirection
@@ -8,7 +9,6 @@ import { useNavigate } from "react-router";
  */
 
 const Home= ( ) => {
-
     const navigate = useNavigate();
 
     /**
@@ -21,12 +21,13 @@ const Home= ( ) => {
         e.preventDefault(); // Le comportement par défaut d'une page html est de recharger la page et d'envoyer les données dans l'URL, donc il faut enlever cela pour gérer nous même le comportement et afficher sur la page le message sans la recharger.
 
         localStorage.removeItem("token");
-        localStorage.setItem("deconnected", "true")
+        localStorage.setItem("deconnected", "true");
         navigate('/');
     }
 
     return (
         <>
+            <Header/>
             <h2 className={styles.h1}>Bravo vous êtes connectés ! Features à venir mais la redirection a fonctionnée donc c'est déjà un bon
             début non? </h2>
             <button className={styles.button} onClick={handleClickedButton}>
