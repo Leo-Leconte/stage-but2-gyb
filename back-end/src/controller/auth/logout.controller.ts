9c4416f80943e32a60ba7e539ab7aa19ae649b63
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-22 14:31:53 +0200

    :sparkles: Ajout de la fonctionnalite logout cote back pour les test d integration

--- back-end/src/controller/auth/logout.controller.ts
+++ back-end/src/controller/auth/logout.controller.ts
@@ -1,7 +1,12 @@
-import {Controller, Post} from "@nestjs/common";
-import {LogoutService} from "../../service/auth/logout.service";
+import { Controller, Headers, Post } from '@nestjs/common';
+import { LogoutService } from '../../service/auth/logout.service';
 
 @Controller('api/auth')
 export class LogoutController {
+  constructor(private readonly logoutService: LogoutService) {}
 
+  @Post('logout')
+  async logout(@Headers('authorization') authHeader: string) {
+    return this.logoutService.logout(authHeader);
+  }
 }
