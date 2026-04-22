bf5fd95411f90708c27dc4891dbb36a229ed55d8
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-22 10:57:16 +0200

    :memo: Ajout de documentation

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000) ; // prend soit le port de l'environnement soit 3000
 }
 bootstrap();
