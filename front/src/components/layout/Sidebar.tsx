5f485e82d30e42479c7606f2e59a3c9f65483443
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-22 16:26:37 +0200

    :art: #34 sidebar maj pour avoir le return que si isOpen est true

--- front/src/components/layout/Sidebar.tsx
+++ front/src/components/layout/Sidebar.tsx
@@ -1,33 +1,38 @@
 import styles from "./Sidebar.module.css";
 
-const Sidebar = () => {
+const Sidebar = ({isOpen} : any) => {
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="#rien" title="L'acueil de notre site">
-            Accueil
-          </a>
-        </li>
-        <li>
-          <a
-            href="#rien"
-            title="Liste des stagiaires (vue liste) avec filtres (service, periode, statut, tuteur)"
-          >
-            Stagiaires
-          </a>
-        </li>
-        <li>
-          <a
-            href="#rien"
-            title="Visualisation des stages sur un calendrier interactif (vue mois / semaine)"
-          >
-            Calendrier
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
-
+      <>
+      {isOpen &&
+      <nav>
+        <ul>
+          <li>
+            <a href="#rien" title="L'acueil de notre site">
+              Accueil
+            </a>
+          </li>
+          <li>
+            <a
+                href="#rien"
+                title="Liste des stagiaires (vue liste) avec filtres (service, periode, statut, tuteur)"
+            >
+              Stagiaires
+            </a>
+          </li>
+          <li>
+            <a
+                href="#rien"
+                title="Visualisation des stages sur un calendrier interactif (vue mois / semaine)"
+            >
+              Calendrier
+            </a>
+          </li>
+        </ul>
+      </nav>}
+      </>
+  )
 }
+
+
+export default Sidebar;
