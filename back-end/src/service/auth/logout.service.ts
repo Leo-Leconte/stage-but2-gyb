9fa69d4326871adafdd5ee8a36962c79828b3f3a
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-22 14:31:48 +0200

    :sparkles: Ajout de la fonctionnalite logout cote back pour les test d integration

--- back-end/src/service/auth/logout.service.ts
+++ back-end/src/service/auth/logout.service.ts
@@ -1,10 +1,35 @@
-import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
-
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class LogoutService {
-    async logout() {
-
-
+  async logout(authHeader: string) {
+    if (!authHeader) {
+      throw new BadRequestException('Un token est requis');
     }
+
+    if (!authHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedException('Pas le bon type pour le token');
+    }
+
+    const token = authHeader.slice(7);
+
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) {
+      throw new Error(
+        'JWT_SECRET est manquant dans les variables d environnement',
+      );
+    }
+
+    jwt.verify(token, secret);
+    return {
+      success: true,
+      message: 'Deconnexion reussie',
+    };
+  }
 }
