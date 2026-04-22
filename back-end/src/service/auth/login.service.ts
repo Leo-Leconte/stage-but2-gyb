9ea25fcdea6d3e4cac660c3af149971e9730f2e3
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-22 10:57:04 +0200

    :memo: Ajout de documentation

--- back-end/src/service/auth/login.service.ts
+++ back-end/src/service/auth/login.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
+import { ConfigModule } from '@nestjs/config';
 import { AuthRepository } from '../../repository/auth/AuthRepository';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class LoginService {
@@ -12,22 +14,44 @@ export class LoginService {
             throw new BadRequestException('Un mot de passe et un email sont requis pour pouvoir vous connecter');
         }
 
+        // on verifie que l'utilisateur existe
         const collaborateur = await this.authRepository.findByEmail(email);
 
         if (!collaborateur) {
             throw new UnauthorizedException('Email ou Mot de passe incorrect');
         }
 
+
+        // on vérifie que le mot de passe est correct
         const ok = await bcrypt.compare(mot_de_passe, collaborateur.mot_de_passe);
 
         if (!ok) {
-            throw new UnauthorizedException('Email ou Mot de passe incorrect');
+            throw new UnauthorizedException('Email ou Mot de passe incorrect ');
         }
 
+        // on verifie que le jwt est bien definit dans les variables d'environnement'
+        const secret = process.env.JWT_SECRET;
+        if (!secret) {
+            throw new Error('JWT_SECRET est manquant dans les variables d environnement');
+        }
+
+        // on cree le token
+        const token = jwt.sign(
+            { id_collaborateur: collaborateur.id_collaborateur, email: collaborateur.email },
+            secret,
+            { expiresIn: 3600 }
+        );
+
+        // on retourne le token et le type de token
         return {
             success: true,
-            username: collaborateur.nom,
-            message: 'Vous etes connecter',
-        };
+            data: {
+                Token: token,
+                "type du token": "bearer",
+                "Expire dans (sec)": 60 ,
+                nom_utilisateur: collaborateur.nom
+            },
+            message: "Connexion établie avec succès"
+        }
     }
 }
