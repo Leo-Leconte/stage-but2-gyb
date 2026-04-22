c74ece17796e28ff902a5ab7b7b0c216236433a6
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-22 14:55:37 +0200

    :art: #34 test sidebar

--- front/src/components/layout/Sidebar.tsx
+++ front/src/components/layout/Sidebar.tsx
@@ -3,8 +3,31 @@ import styles from "./Sidebar.module.css";
 const Sidebar = () => {
 
   return (
-
-
-  )
+    <nav>
+      <ul>
+        <li>
+          <a href="#rien" title="L'acueil de notre site">
+            Accueil
+          </a>
+        </li>
+        <li>
+          <a
+            href="#rien"
+            title="Liste des stagiaires (vue liste) avec filtres (service, periode, statut, tuteur)"
+          >
+            Stagiaires
+          </a>
+        </li>
+        <li>
+          <a
+            href="#rien"
+            title="Visualisation des stages sur un calendrier interactif (vue mois / semaine)"
+          >
+            Calendrier
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 
 }
