b3557aa2893d43c7c217ad731195031af3f13095
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-23 14:56:16 +0200

    :sparkles: #51 CRUD en ts du stagiaire fait (avec sa TSDoc) avec awaits pr les erreurs

---
+++ back-end/src/service/stagiaire/stagiaire.service.ts
@@ -0,0 +1,83 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { StagiaireRepositoryCrud } from 'src/repository/stagiaire/StagiaireRepositoryCrud';
+
+@Injectable()
+export class StagiaireService {
+    constructor(private readonly stagiaireRepositoryCrud: StagiaireRepositoryCrud) {}
+
+    /**
+     * Permet de recuperer tous les stagiaires
+     */
+    async findAll() {
+        return this.stagiaireRepositoryCrud.findAll();
+    }
+
+    /**
+     * Permet de recuperer un stagiaire par son id
+     * @param id
+     * le if present permet de verifie si le stagiaire existe
+     */
+    async findByid(id: number) {
+        const idS = await this.stagiaireRepositoryCrud.findById(id);
+
+        if (!idS) {
+            throw new NotFoundException('stagiaire introuvable');
+        }
+        return {
+            message: `stagiaire ${id} trouvé`,
+            stagiaire: idS,
+        };
+    }
+
+    /**
+     * Permet de supprimer un stagiaire par son id
+     * @param id
+     * le if present permet de verifie si le stagiaire existe
+     */
+
+    async delete(id: number) {
+        const idS = await this.stagiaireRepositoryCrud.findById(id);
+
+        if (!idS) {
+            throw new NotFoundException(`stagiaire introuvable`);
+        }
+
+        await this.stagiaireRepositoryCrud.delete(id);
+
+        return {
+            message: `Le stagiaire ${id} a été supprimé avec succès `,
+        };
+    }
+
+    /**
+     * Permet de modifier un stagiaire par son id
+     * @param id
+     * @param stagiaire
+     * le if present permet de verifie si le stagiaire existe
+     */
+    async update(id: number, stagiaire: any) {
+        const idS = await this.stagiaireRepositoryCrud.findById(id);
+
+        if (!idS) {
+            throw new NotFoundException(`stagiaire introuvable`);
+        }
+
+        await this.stagiaireRepositoryCrud.update(id, stagiaire);
+
+        return {
+            message: `Le stagiaire ${id} a été mis à jour avec succès`,
+        };
+    }
+
+    /**
+     * Permet de creer un stagiaire
+     * @param stagiaire
+     */
+    async create(stagiaire: any) {
+        await this.stagiaireRepositoryCrud.create(stagiaire);
+
+        return {
+            message: 'stagiaire crée avec succès',
+        };
+    }
+}
