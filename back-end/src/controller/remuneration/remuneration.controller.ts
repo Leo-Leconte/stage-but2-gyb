3d5f18fe0dc0e29f2d08b42a5447d2bfb06893e4
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-23 15:40:48 +0200

    :art: ajout de try catch pour avoir un acces au log d erreur

--- back-end/src/controller/remuneration/remuneration.controller.ts
+++ back-end/src/controller/remuneration/remuneration.controller.ts
@@ -15,27 +15,50 @@ export class RemunerationController {
   constructor(private readonly remunerationService: RemunerationService) {}
 
   @Get()
-  findAll() {
-    return this.remunerationService.findAll();
+  async findAll() {
+    try {
+      return await this.remunerationService.findAll();
+    } catch (error) {
+      console.error('Erreur findAll remuneration', error);
+    }
   }
 
   @Get(':id')
-  findById(@Param('id', ParseIntPipe) id: number) {
-    return this.remunerationService.findById(id);
+  async findById(@Param('id', ParseIntPipe) id: number) {
+    try {
+      return await this.remunerationService.findById(id);
+    } catch (error) {
+      console.error('Erreur findById remuneration', error);
+    }
   }
 
   @Delete(':id')
-  delete(@Param('id', ParseIntPipe) id: number) {
-    return this.remunerationService.delete(id);
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    try {
+      return await this.remunerationService.delete(id);
+    } catch (error) {
+      console.error('Erreur delete remuneration', error);
+    }
   }
 
   @Post('/create')
-  create(@Body() remuneration: any) {
-    return this.remunerationService.create(remuneration);
+  async create(@Body() remuneration: any) {
+    try {
+      return await this.remunerationService.create(remuneration);
+    } catch (error) {
+      console.error('Erreur create remuneration', error);
+    }
   }
 
   @Put(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() remuneration: any) {
-    return this.remunerationService.update(id, remuneration);
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() remuneration: any,
+  ) {
+    try {
+      return await this.remunerationService.update(id, remuneration);
+    } catch (error) {
+      console.error('Erreur update remuneration', error);
+    }
   }
 }
