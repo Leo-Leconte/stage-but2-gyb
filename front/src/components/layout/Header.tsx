0bfa0bb9ca5610a73b46c9b3e3001a097996ea43
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-23 11:05:30 +0200

    :art: #34 header modif pour renvoyer vers home quand on clique sur le logo

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -1,7 +1,14 @@
 import logoGyb from "../../assets/logo-GYB.png";
 import styles from "./Header.module.css"
+import { useNavigate } from "react-router";
 
 const Header = ({isOpen, setIsOpen} : any) => {
+
+    const navigate = useNavigate();
+    function handleLogo(){
+        navigate("/home");
+    }
+
     return (
         <>
             <header className={styles.header}>
@@ -10,7 +17,7 @@ const Header = ({isOpen, setIsOpen} : any) => {
                     <span></span>
                     <span></span>
                 </button>
-                <img src={logoGyb} alt="logo gyb" className={styles.logoGyb} />
+                <img src={logoGyb} alt="logo gyb" className={styles.logoGyb} onClick={handleLogo}/>
             </header>
 
         </>
