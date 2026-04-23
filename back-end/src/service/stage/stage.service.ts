38fa6ddad43f1f65cc0551b8b248f81161187104
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-23 14:03:30 +0200

    :art: correction de bug

--- back-end/src/service/stage/stage.service.ts
+++ back-end/src/service/stage/stage.service.ts
@@ -1,27 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { StageRepositoryCrud } from 'src/repository/stage/StageRpositoryCrud';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { StageRepositoryCrud } from 'src/repository/stage/StageRepositoryCrud';
 
 @Injectable()
 export class StageService {
   constructor(private readonly stageRepositoryCrud: StageRepositoryCrud) {}
 
-  findAll() {
+  async findAll() {
     return this.stageRepositoryCrud.findAll();
   }
 
-  findByid(id: number) {
-    return this.stageRepositoryCrud.findById(id);
+  async findByid(id: number) {
+    const idS = await this.stageRepositoryCrud.findById(id);
+
+    if (!idS) {
+      throw new NotFoundException('Stage introuvable');
+    }
+    return {
+      message: `Stage ${id} trouvé`,
+      stage: idS,
+    };
   }
 
-  delete(id: number) {
-    return this.stageRepositoryCrud.delete(id);
+  async delete(id: number) {
+    const idS = await this.stageRepositoryCrud.findById(id);
+
+    if (!idS) {
+      throw new NotFoundException(`Stage introuvable`);
+    }
+
+    await this.stageRepositoryCrud.delete(id);
+
+    return {
+      message: `Le Stage ${id} a été supprimé avec succès `,
+    };
   }
 
-  update(id: number, stage: any) {
-    return this.stageRepositoryCrud.update(id, stage);
+  async update(id: number, stage: any) {
+    const idS = await this.stageRepositoryCrud.findById(id);
+
+    if (!idS) {
+      throw new NotFoundException(`Stage introuvable`);
+    }
+
+    await this.stageRepositoryCrud.update(id, stage);
+
+    return {
+      message: `Le Stage ${id} a été mis à jour avec succès`,
+    };
   }
 
-  create(stage: any) {
-    return this.stageRepositoryCrud.create(stage);
+  async create(stage: any) {
+    await this.stageRepositoryCrud.create(stage);
+
+    return {
+      message: 'Stage crée avec succès',
+    };
   }
 }
