14a8b595c68e5b8f21acab2a3a78d4a9bbffe367
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-23 10:47:30 +0200

    :art: #34 J'enlève le bouton de home tsx pour le mettre dans la sidebar, ça me parait plus logique

--- front/src/components/home/Home.tsx
+++ front/src/components/home/Home.tsx
@@ -1,5 +1,3 @@
-import styles from "../login/Login.module.css";
-import { useNavigate } from "react-router";
 import Header from "../layout/Header.tsx";
 import Sidebar from "../layout/Sidebar.tsx";
 import {useState} from "react";
@@ -12,7 +10,6 @@ import {useState} from "react";
  */
 
 const Home= ( ) => {
-    const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
 
     /**
@@ -21,23 +18,12 @@ const Home= ( ) => {
      * Le renvoie vers la page de login
      * @param e ; enlève le comportement de la page par défaut
      */
-    async function handleClickedButton(e : any) {
-        e.preventDefault(); // Le comportement par défaut d'une page html est de recharger la page et d'envoyer les données dans l'URL, donc il faut enlever cela pour gérer nous même le comportement et afficher sur la page le message sans la recharger.
-
-        localStorage.removeItem("access_token");
-        localStorage.setItem("deconnected", "true");
-        navigate('/');
-    }
 
     return (
         <>
-            {/*Je savais pas trop si c'est possible de mettre ça dans le CSS alors je l'ai mis ici*/}
-            <div style={{ marginLeft: isOpen ? '250px' : '0', transition: 'margin 0.3s ease' }}>
-            <Header setIsOpen={setIsOpen} isOpen={isOpen}/>
             <Sidebar isOpen={isOpen}/>
-            <button className={styles.button} onClick={handleClickedButton}>
-                Se déconnecter
-            </button>
+            <div style={{ marginLeft: isOpen ? '250px' : '0', transition: 'margin 0.3s ease' }}>
+                <Header setIsOpen={setIsOpen} isOpen={isOpen}/>
             </div>
         </>
     )
