6ed6b2cf17fc76ae8cf0f2ef51cfe8c16eb85ba5
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-23 15:56:28 +0200

    :sparkles: #51 module modifié pr le CRUD stagiaire

---
+++ back-end/src/module/stagiaire/stagiaire.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stagiaire } from '../../repository/StagiaireRepository';
+import { StageController } from '../../controller/stage/stage.controller';
+import {StagiaireService} from "../../service/stagiaire/stagiaire.service";
+import { StagiaireRepositoryCrud } from '../../repository/stagiaire/StagiaireRepositoryCrud';
+import { StagiaireController } from '../../controller/stagiaire/stagiaire.controller';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Stagiaire])],
+    controllers: [StagiaireController],
+    providers: [StagiaireService, StagiaireRepositoryCrud],
+})
+export class StagiaireModule {}
