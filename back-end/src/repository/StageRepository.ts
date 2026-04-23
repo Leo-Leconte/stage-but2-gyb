7fbc0cd3a3372c393613c608a9cf5f42d6a5d0a4
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-23 11:58:48 +0200

    :sparkles: Modification du de la bd pour que le crud stage Delete marche car y avait erreur de cle etranger

--- back-end/src/repository/StageRepository.ts
+++ back-end/src/repository/StageRepository.ts
@@ -4,39 +4,42 @@ import { Stagiaire } from './StagiaireRepository';
 import { Tuteur } from './TuteurRepository';
 import { Remuneration } from './RemunerationRepository';
 
-@Entity ({ schema: 'gyb_stage', name: 'stage' })
+@Entity({ schema: 'gyb_stage', name: 'stage' })
 export class Stage {
-
-  @PrimaryGeneratedColumn({name : 'id_stage'})
+  @PrimaryGeneratedColumn({ name: 'id_stage' })
   id: number;
 
-  @Column({ type: 'varchar', length: 100 , name : 'intitule'})
+  @Column({ type: 'varchar', length: 100, name: 'intitule' })
   intitule: string;
 
-  @Column({ type: 'varchar', length: 100 , name : 'description_missions' })
+  @Column({ type: 'varchar', length: 100, name: 'description_missions' })
   description_missions: string;
 
-  @Column({ type: 'varchar', length: 100 , name : 'developpement_competences' })
+  @Column({ type: 'varchar', length: 100, name: 'developpement_competences' })
   developpement_competences: string;
 
-  @Column({ type: 'varchar', length: 100 , name : 'service_accueil'})
+  @Column({ type: 'varchar', length: 100, name: 'service_accueil' })
   service_accueil: string;
 
-  @Column({ type: 'enum', enum: ['a venir', 'en cours', 'termine'] , name : 'statut'})
+  @Column({
+    type: 'enum',
+    enum: ['a venir', 'en cours', 'termine'],
+    name: 'statut',
+  })
   statut: string;
 
-  @Column({ type: 'date' , name : 'date_debut'})
+  @Column({ type: 'date', name: 'date_debut' })
   date_debut: string;
 
-  @Column({ type: 'date' , name : 'date_fin'})
+  @Column({ type: 'date', name: 'date_fin' })
   date_fin: string;
 
-  @ForeignKey(() => Stagiaire)
+  @Column({ type: 'int', name: 'id_stagiaire', nullable: true })
   id_stagiaire: number;
 
-  @ForeignKey(() => Tuteur)
+  @Column({ type: 'int', name: 'id_tuteur', nullable: true })
   id_tuteur: number;
 
-  @ForeignKey(() => Remuneration)
+  @Column({ type: 'int', name: 'id_remuneration', nullable: true })
   id_remuneration: number;
 }
