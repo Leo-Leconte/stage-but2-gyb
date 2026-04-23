582438553653c22f7c2d3e5c7b18d35df7907434
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-23 10:47:44 +0200

    :art: #34 J'ajoute le bouton dans la sidebar

--- front/src/components/layout/Sidebar.tsx
+++ front/src/components/layout/Sidebar.tsx
@@ -1,6 +1,15 @@
 import styles from "./Sidebar.module.css";
-
+import { useNavigate } from "react-router";
 const Sidebar = ({isOpen} : any) => {
+    const navigate = useNavigate();
+
+    async function handleClickedButton(e : any) {
+        e.preventDefault(); // Le comportement par défaut d'une page html est de recharger la page et d'envoyer les données dans l'URL, donc il faut enlever cela pour gérer nous même le comportement et afficher sur la page le message sans la recharger.
+
+        localStorage.removeItem("access_token");
+        localStorage.setItem("deconnected", "true");
+        navigate('/');
+    }
 
   return (
       <>
@@ -23,6 +32,12 @@ const Sidebar = ({isOpen} : any) => {
             <a href="#rien" title="Visualisation des stages sur un calendrier interactif (vue mois / semaine)">
               Calendrier
             </a>
+          </div>
+
+          <div className={styles.deconnexion}>
+          <button onClick={handleClickedButton}>
+              Se déconnecter
+          </button>
           </div>
 
       </nav>}
