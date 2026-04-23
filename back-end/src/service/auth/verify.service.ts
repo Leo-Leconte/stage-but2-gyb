c7a5b7bdcb0fa092615e827e7bbf80c16c41a3e2
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-23 11:05:20 +0200

    :bug: Le verify disait que meme deco le collaborateur est toujours connecter

--- back-end/src/service/auth/verify.service.ts
+++ back-end/src/service/auth/verify.service.ts
@@ -1,40 +1,56 @@
-import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthRepository } from '../../repository/auth/AuthRepository';
+import { BlacklistService } from './blacklist.service';
 import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class VerifyService {
-    constructor(private readonly authRepository: AuthRepository) {}
+  constructor(
+    private readonly authRepository: AuthRepository,
+    private readonly blacklistService: BlacklistService,
+  ) {}
 
-    async verify(authHeader: string) {
-
-        const secret = process.env.JWT_SECRET;
-        if (!secret) {
-            throw new Error('JWT_SECRET est manquant dans les variables d environnement');
-        }
-        // verification du token s'il est fourni
-        if (!authHeader) {
-            throw new BadRequestException('Un token est requis pour pouvoir vous connecter');
-        }
-
-        // vérifie le type du token
-        if (!authHeader.startsWith("Bearer")) {
-            throw new UnauthorizedException('Pas le bon type pour le token');
-        }
-
-        const token = authHeader.slice(7);
-
-        // verification du token
-        try {
-            return jwt.verify(token, secret);
-
-        }catch (err){
-            // si le token est expiré
-            if (err.name === "TokenExpiredError") {
-                throw new UnauthorizedException('Token expiré, reconnectez-vous');
-            }
-            // si le token est invalide
-            throw new UnauthorizedException('Token invalide');
-        }
+  async verify(authHeader: string) {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error(
+        'JWT_SECRET est manquant dans les variables d environnement',
+      );
     }
+    // verification du token s'il est fourni
+    if (!authHeader) {
+      throw new BadRequestException(
+        'Un token est requis pour pouvoir vous connecter',
+      );
+    }
+
+    // vérifie le type du token
+    if (!authHeader.startsWith('Bearer')) {
+      throw new UnauthorizedException('Pas le bon type pour le token');
+    }
+
+    const token = authHeader.slice(7);
+
+    // si le token est black list alors c'est qu'il est deja déconnecté
+    if (this.blacklistService.has(token)) {
+      throw new UnauthorizedException(
+        'Vous avez ete deconnecte, veuillez vous reconnecter',
+      );
+    }
+    // verification du token
+    try {
+      return jwt.verify(token, secret);
+    } catch (err) {
+      // si le token est expiré
+      if (err.name === 'TokenExpiredError') {
+        throw new UnauthorizedException('Token expiré, reconnectez-vous');
+      }
+      // si le token est invalide
+      throw new UnauthorizedException('Token invalide');
+    }
+  }
 }
