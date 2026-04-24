f3fa617fbcb32e182e3de8f46e0c3732c18636da
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-24 14:04:24 +0200

    :zap:   #53 ajout d'un fichier pour les stages provisoiremment

---
+++ front/src/components/stagesProvisoires/StagesProvisoires.tsx
@@ -0,0 +1,8 @@
+const StagesProvisoires = () => {
+    return (
+        <>
+            <h2>Bienvenue sur la page avec la fiche des stages </h2>
+        </>
+    );
+};
+export default StagesProvisoires;
