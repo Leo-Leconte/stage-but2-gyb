4a49ef735da3afb8219dc4f07e55fdb7af2dc067
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-24 10:20:41 +0200

    :sparkles: Adaptation du nouveau policy

--- back-end/src/module/remuneration/remuneration.module.ts
+++ back-end/src/module/remuneration/remuneration.module.ts
@@ -4,9 +4,10 @@ import { Remuneration } from '../../repository/RemunerationRepository';
 import { RemunerationController } from '../../controller/remuneration/remuneration.controller';
 import { RemunerationService } from '../../service/remuneration/remuneration.service';
 import { RemunerationRepositoryCrud } from '../../repository/remuneration/RemunerationRepositoryCrud';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Remuneration])],
+  imports: [TypeOrmModule.forFeature([Remuneration]), AuthModule],
   controllers: [RemunerationController],
   providers: [RemunerationService, RemunerationRepositoryCrud],
 })
