76a0bd0f1909192f4ee25e474132068a9a5446eb
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-24 15:43:16 +0200

    :art: permet d acceder a la creation des stagiaire

--- front/src/components/stagiaires/Stagiaires.tsx
+++ front/src/components/stagiaires/Stagiaires.tsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router";
+
 const Stagiaires = () => {
+  const navigate = useNavigate();
+  function handleCreate() {
+    navigate("../crud/create/stagiaires");
+  }
   return (
     <>
       <h2>Bienvenue sur la page stagiaires </h2>
+      <button onClick={handleCreate}>Crée un stagiaire</button>
     </>
   );
 };
