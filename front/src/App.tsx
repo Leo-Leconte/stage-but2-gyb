eff168cf1e735b3e755b59e1b39ad28dad13d709
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-24 15:42:35 +0200

    :art: Ajout de la route pour la creation

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,29 @@
-import './App.css'
-import Login from './components/login/Login.tsx'
-import Home from './components/home/Home.tsx'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./App.css";
+import Login from "./components/login/Login.tsx";
+import Home from "./components/home/Home.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout.tsx";
 import Stagiaires from "./components/stagiaires/Stagiaires.tsx";
 import Calendrier from "./components/calendrier/Calendrier.tsx";
-
+import CreateNewStagiaires from "./components/crud/create/stagiaires.tsx";
 
 function App() {
-
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path={"/"} element={<Login/>} />
-              <Route element={<Layout/>}>
-                  <Route path="/home" element={<Home/>} />
-                  <Route path="/stagiaires" element={<Stagiaires/>} />
-                  <Route path="/calendrier" element={<Calendrier/>} />
-              </Route>
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Login />} />
+        <Route
+          path={"/crud/create/stagiaires"}
+          element={<CreateNewStagiaires />}
+        />
+        <Route element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/stagiaires" element={<Stagiaires />} />
+          <Route path="/calendrier" element={<Calendrier />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
