b7c5ea1c4f24fc905a766dde86aa0fdc45e90e69
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-27 09:43:58 +0200

    :sparkles: #53 mini modif pour voir un peu comment ça va se rérouler

---
+++ front/src/components/stageDetails/StageDetails.tsx
@@ -0,0 +1,35 @@
+import { useParams } from "react-router";
+
+const StagesProvisoires = () => {
+
+
+    return (
+        <>
+            <h2>Bienvenue sur la page des détails du stage </h2>
+
+            {
+                /*
+            stage.map(stage:any) => (
+                <div>
+                    <div>date de début de stage : {stage.date_debut}</div>
+                    <div>date de fin de stage : {stage.date_fin}</div>
+                    <div>service d'accueil : {stage.service_accueil}</div>
+                    <div>statut : {stage.statut}</div>
+                    <div>id du stagiaire : {stage.id_stagiaire}</div>
+                    <div>id du tuteur : {stage.id_tuteur}</div>
+                    <div>id de rémunération : {stage.id_remuneration}</div>
+                    <p>================</p>
+                </div>
+
+                )
+                */
+            }
+
+
+
+
+
+        </>
+    );
+};
+export default StagesProvisoires;
