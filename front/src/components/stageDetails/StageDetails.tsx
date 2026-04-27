06e5e5b73479263faaf27e70c6561a95b68182fe
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-27 10:46:41 +0200

    :sparkles: #53 Détails d'un stage fonctionnel

--- front/src/components/stageDetails/StageDetails.tsx
+++ front/src/components/stageDetails/StageDetails.tsx
@@ -1,15 +1,57 @@
 import { useParams } from "react-router";
+import {useEffect, useState} from "react";
+
+//import {Route} from "react-router-dom";
 
 const StagesProvisoires = () => {
+
+    type StageType = {
+        id: number;
+        intitule: string;
+        description_missions: string;
+        developpement_competences: string;
+        date_debut: string;
+        date_fin: string;
+        service_accueil: string;
+        statut: string;
+        id_stagiaire: number;
+        id_tuteur: number;
+        id_remuneration: number;
+    }
+
+    //<Route path="http://localhost:5173/stage/:postId" element=<Post/> />;
+    const [stage, setStage] = useState<StageType | null>(null); // Je l'initialse de type StageType ou null je trouve ça plus pro qu'un any
+
+    const { id } = useParams(); // Il faut le même nom que dans App sinon c'est undefined somewhy
+
+
+    useEffect(() =>{ // dans un useEffect pour que ça fasse le fetch à chaque fois que le page se charge
+        async function fetching(){
+
+            const token=localStorage.getItem("access_token"); // Faut récup et envoyer le token vu que la policy a été mise en place mtn :(
+            let reponseApi = await fetch(`http://127.0.0.1:3000/api/stage/${id}`, {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+            const content = await reponseApi.json();
+            setStage(content.stage);
+            console.log(content.stage);
+        }
+        fetching();
+    },[])
+
 
 
     return (
         <>
             <h2>Bienvenue sur la page des détails du stage </h2>
 
-            {
-                /*
-            stage.map(stage:any) => (
+            {stage && (
+
                 <div>
                     <div>date de début de stage : {stage.date_debut}</div>
                     <div>date de fin de stage : {stage.date_fin}</div>
@@ -20,9 +62,7 @@ const StagesProvisoires = () => {
                     <div>id de rémunération : {stage.id_remuneration}</div>
                     <p>================</p>
                 </div>
-
-                )
-                */
+            )
             }
 
 
