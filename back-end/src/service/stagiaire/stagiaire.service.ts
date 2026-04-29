eadd70407275e68fce5fb0a33a6e9107fbcaaccd
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-29 09:57:11 +0200

    :ambulance: Changement pour retoruner l id en plus

--- back-end/src/service/stagiaire/stagiaire.service.ts
+++ back-end/src/service/stagiaire/stagiaire.service.ts
@@ -3,81 +3,84 @@ import { StagiaireRepositoryCrud } from 'src/repository/stagiaire/StagiaireRepos
 
 @Injectable()
 export class StagiaireService {
-    constructor(private readonly stagiaireRepositoryCrud: StagiaireRepositoryCrud) {}
+  constructor(
+    private readonly stagiaireRepositoryCrud: StagiaireRepositoryCrud,
+  ) {}
 
-    /**
-     * Permet de recuperer tous les stagiaires
-     */
-    async findAll() {
-        return this.stagiaireRepositoryCrud.findAll();
+  /**
+   * Permet de recuperer tous les stagiaires
+   */
+  async findAll() {
+    return this.stagiaireRepositoryCrud.findAll();
+  }
+
+  /**
+   * Permet de recuperer un stagiaire par son id
+   * @param id
+   * le if present permet de verifie si le stagiaire existe
+   */
+  async findByid(id: number) {
+    const idS = await this.stagiaireRepositoryCrud.findById(id);
+
+    if (!idS) {
+      throw new NotFoundException('stagiaire introuvable');
+    }
+    return {
+      message: `stagiaire ${id} trouvé`,
+      stagiaire: idS,
+    };
+  }
+
+  /**
+   * Permet de supprimer un stagiaire par son id
+   * @param id
+   * le if present permet de verifie si le stagiaire existe
+   */
+
+  async delete(id: number) {
+    const idS = await this.stagiaireRepositoryCrud.findById(id);
+
+    if (!idS) {
+      throw new NotFoundException(`stagiaire introuvable`);
     }
 
-    /**
-     * Permet de recuperer un stagiaire par son id
-     * @param id
-     * le if present permet de verifie si le stagiaire existe
-     */
-    async findByid(id: number) {
-        const idS = await this.stagiaireRepositoryCrud.findById(id);
+    await this.stagiaireRepositoryCrud.delete(id);
 
-        if (!idS) {
-            throw new NotFoundException('stagiaire introuvable');
-        }
-        return {
-            message: `stagiaire ${id} trouvé`,
-            stagiaire: idS,
-        };
+    return {
+      message: `Le stagiaire ${id} a été supprimé avec succès `,
+    };
+  }
+
+  /**
+   * Permet de modifier un stagiaire par son id
+   * @param id
+   * @param stagiaire
+   * le if present permet de verifie si le stagiaire existe
+   */
+  async update(id: number, stagiaire: any) {
+    const idS = await this.stagiaireRepositoryCrud.findById(id);
+
+    if (!idS) {
+      throw new NotFoundException(`stagiaire introuvable`);
     }
 
-    /**
-     * Permet de supprimer un stagiaire par son id
-     * @param id
-     * le if present permet de verifie si le stagiaire existe
-     */
+    await this.stagiaireRepositoryCrud.update(id, stagiaire);
 
-    async delete(id: number) {
-        const idS = await this.stagiaireRepositoryCrud.findById(id);
+    return {
+      message: `Le stagiaire ${id} a été mis à jour avec succès`,
+    };
+  }
 
-        if (!idS) {
-            throw new NotFoundException(`stagiaire introuvable`);
-        }
+  /**
+   * Permet de creer un stagiaire
+   * @param stagiaire
+   */
+  async create(stagiaire: any) {
+    const Stagiaire = await this.stagiaireRepositoryCrud.create(stagiaire);
 
-        await this.stagiaireRepositoryCrud.delete(id);
-
-        return {
-            message: `Le stagiaire ${id} a été supprimé avec succès `,
-        };
-    }
-
-    /**
-     * Permet de modifier un stagiaire par son id
-     * @param id
-     * @param stagiaire
-     * le if present permet de verifie si le stagiaire existe
-     */
-    async update(id: number, stagiaire: any) {
-        const idS = await this.stagiaireRepositoryCrud.findById(id);
-
-        if (!idS) {
-            throw new NotFoundException(`stagiaire introuvable`);
-        }
-
-        await this.stagiaireRepositoryCrud.update(id, stagiaire);
-
-        return {
-            message: `Le stagiaire ${id} a été mis à jour avec succès`,
-        };
-    }
-
-    /**
-     * Permet de creer un stagiaire
-     * @param stagiaire
-     */
-    async create(stagiaire: any) {
-        await this.stagiaireRepositoryCrud.create(stagiaire);
-
-        return {
-            message: 'stagiaire crée avec succès',
-        };
-    }
+    return {
+      message: 'stagiaire crée avec succès',
+      id: Stagiaire.id,
+    };
+  }
 }
