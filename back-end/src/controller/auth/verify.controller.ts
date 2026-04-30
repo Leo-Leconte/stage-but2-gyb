59895b46bd51c0001e75c5816a6f433b590a7942
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-30 14:59:28 +0200

    :sparkles: Ajout de la doc swagger API pour l'api Rest

--- back-end/src/controller/auth/verify.controller.ts
+++ back-end/src/controller/auth/verify.controller.ts
@@ -1,27 +1,47 @@
-import { Controller, Get, Headers, UnauthorizedException } from "@nestjs/common";
-import {VerifyService} from "../../service/auth/verify.service";
+import {
+  Controller,
+  Get,
+  Headers,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { VerifyService } from '../../service/auth/verify.service';
+import {
+  ApiBearerAuth,
+  ApiHeaders,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('api/auth')
 export class VerifyController {
+  constructor(readonly VerifyService: VerifyService) {}
 
-    constructor(readonly VerifyService: VerifyService) {}
+  /**
+   * Permet de verifier le token
+   *
+   * @path {GET} /api/auth/me
+   *
+   * Check si le token est valide
+   * @param authHeader ; le token
+   */
 
-    /**
-     * Permet de verifier le token
-     *
-     * @path {GET} /api/auth/me
-     *
-     * Check si le token est valide
-     * @param authHeader ; le token
-     */
-
-    @Get('me')
-    async verify(@Headers('authorization') authHeader: string) {
-        try {
-            return await this.VerifyService.verify(authHeader);
-        } catch (error) {
-            console.error('Erreur verify:', error);
-            throw error;
-        }
+  @ApiOperation({ summary: 'Verification du token' })
+  @ApiBearerAuth()
+  @Get('me')
+  @ApiResponse({
+    status: 200,
+    description: 'Token valide',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Token invalide',
+  })
+  async verify(@Headers('authorization') authHeader: string) {
+    try {
+      return await this.VerifyService.verify(authHeader);
+    } catch (error) {
+      console.error('Erreur verify:', error);
+      throw error;
     }
+  }
 }
