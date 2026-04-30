39849d016e4efb155ea5fcd05106b858c200d0ab
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-04-30 14:58:42 +0200

    :sparkles: Ajout de la doc swagger API pour l'api Rest

--- back-end/src/controller/stage/stage.controller.ts
+++ back-end/src/controller/stage/stage.controller.ts
@@ -11,13 +11,25 @@ import {
 } from '@nestjs/common';
 import { StageService } from '../../service/stage/stage.service';
 import { CollaborateurRoleGuard } from '../../Guard/collaborateurRole.guard';
-import {CreateStageDto} from "../../repository/stage/dto/stage.dto";
-
+import { CreateStageDto } from '../../repository/stage/dto/stage.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 @Controller('api/stage')
 export class StageController {
   constructor(private readonly stageService: StageService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Récupérer tous les stages' })
+  @ApiResponse({
+    status: 200,
+    description: 'Liste des stages récupérée avec succès.',
+  })
+  @ApiResponse({ status: 403, description: 'Accès refusé.' })
   @Get()
   @UseGuards(CollaborateurRoleGuard)
   async findAll() {
@@ -28,6 +40,10 @@ export class StageController {
     }
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Recuperer un stage par son id' })
+  @ApiResponse({ status: 200, description: 'Stage récupéré avec succès' })
+  @ApiResponse({ status: 403, description: 'Accès refusé' })
   @Get(':id')
   @UseGuards(CollaborateurRoleGuard)
   async findById(@Param('id', ParseIntPipe) id: number) {
@@ -38,6 +54,10 @@ export class StageController {
     }
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Supprimer un stage par son id' })
+  @ApiResponse({ status: 200, description: 'Suppression du stage avec succes' })
+  @ApiResponse({ status: 403, description: 'Accès refusé' })
   @Delete(':id')
   @UseGuards(CollaborateurRoleGuard)
   async delete(@Param('id', ParseIntPipe) id: number) {
@@ -48,6 +68,12 @@ export class StageController {
     }
   }
 
+  // pour le apibody pas obliger on peut faire la creation direct dans swagger
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Creer un stage' })
+  @ApiResponse({ status: 200, description: 'Ajout du stage avec succes' })
+  @ApiResponse({ status: 403, description: 'Accès refusé' })
+  @ApiBody({})
   @Post('/create')
   @UseGuards(CollaborateurRoleGuard)
   async create(@Body() stage: CreateStageDto) {
@@ -58,9 +84,21 @@ export class StageController {
     }
   }
 
+  // pour le apibody pas obliger on peut faire la creation direct dans swagger
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Modifier un stage par son id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Modification du stage avec succes',
+  })
+  @ApiResponse({ status: 403, description: 'Accès refusé' })
+  @ApiBody({})
   @Put(':id')
   @UseGuards(CollaborateurRoleGuard)
-  async update(@Param('id', ParseIntPipe) id: number, @Body() stage: CreateStageDto) {
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() stage: CreateStageDto,
+  ) {
     try {
       return await this.stageService.update(id, stage);
     } catch (error) {
