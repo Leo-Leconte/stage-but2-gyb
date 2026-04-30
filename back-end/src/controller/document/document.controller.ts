7d34acf784420b42421643dffd4e3de27f1ff419
leo_leconte <leo_leconte@ens.univ-artois.fr>
2026-04-30 15:29:57 +0200

    :sparkles: #80 document controller

---
+++ back-end/src/controller/document/document.controller.ts
@@ -0,0 +1,36 @@
+import {
+    Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    UseGuards,
+} from '@nestjs/common';
+import { DocumentService } from '../../service/document/document.service';
+import { CollaborateurRoleGuard } from '../../Guard/collaborateurRole.guard';
+
+
+@Controller('api/document')
+export class DocumentController {
+    constructor(private readonly documentService: DocumentService) {
+    }
+
+    @Get()
+    @UseGuards(CollaborateurRoleGuard)
+    async findAll() {
+        try {
+            return await this.documentService.findAll();
+        } catch (error) {
+            console.error('Erreur findAll document', error);
+        }
+    }
+
+    @Get(':id')
+    @UseGuards(CollaborateurRoleGuard)
+    async findById(@Param('id', ParseIntPipe) id: number) {
+        try {
+            return await this.documentService.findByid(id);
+        } catch (error) {
+            console.error('Erreur findById document', error);
+        }
+    }
+}
