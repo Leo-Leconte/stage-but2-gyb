e6defe968bc7a302b55308cfa6007f5a43bd9fe0
Rabhi Nessim <nessim_rabhi@ens.univ-artois.fr>
2026-05-04 10:35:34 +0200

    :memo: Vérification du code et ajout de la tsdoc si elle était manquante

--- back-end/src/service/auth/blacklist.service.ts
+++ back-end/src/service/auth/blacklist.service.ts
@@ -3,18 +3,38 @@ import { Injectable } from '@nestjs/common';
 export class BlacklistService {
   private readonly blacklisted: Set<string> = new Set();
 
+  /**
+   * Ajoute le token dans la blacklist
+   *
+   * @param token
+   */
   add(token: string): void {
     this.blacklisted.add(token);
   }
 
+  /**
+   * Verifie si le token est dans la blacklist
+   *
+   * Si le token est dans la blacklist, renvoie true
+   * Sinon renvoie false
+   * @param token
+   */
   has(token: string): boolean {
     return this.blacklisted.has(token);
   }
 
+  /**
+   * Supprime le token de la blacklist
+   *
+   * @param token
+   */
   remove(token: string): void {
     this.blacklisted.delete(token);
   }
 
+  /**
+   * Vide la blacklist quand on lance l'application'
+   */
   clear(): void {
     this.blacklisted.clear();
   }
